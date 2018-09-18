559cbe7e70a43b43c63ef2f017a3d480e9496909
Nicholas Blott <blottn@tcd.ie>
2018-09-18 12:12:05 +0100

    changed title

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_SINGLE);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("OpenGL - Creating a triangle");
+    glutCreateWindow("Opengl - quickstart");
     glutDisplayFunc(drawTriangle);
     glutMainLoop();
     return 0;
